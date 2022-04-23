e6e8f89f97651aac45bb5800f84d8fff07860510
Devilper <50566539+Devilper@users.noreply.github.com>
2022-04-23 18:37:53 +0800

    debug

--- factory_django/settings.py
+++ factory_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%t(%c-t4p%uq7g90=#t_i5=@a77yy^oa_42oouj$750%r25)7)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
